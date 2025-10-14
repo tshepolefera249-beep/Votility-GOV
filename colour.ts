d14a240e6ca3ce3d2772b962baee93a724f77681
tshepolefera249-beep <tshepolefera249@gmail.com>
2025-10-14 09:56:45 +0200

    Create colour.ts

---
+++ colour.ts
@@ -0,0 +1,10 @@
+export default {
+  primary: '#007BFF',
+  black: '#000000',
+  white: '#FFFFFF',
+  grey: '#6C757D',
+  lightGrey: '#F8F9FA',
+  success: '#28A745',
+  danger: '#DC3545',
+  border: '#DEE2E6',
+};
