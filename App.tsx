7dc02e10a4d80aee2a4eef50ec19258e221d1a00
tshepolefera249-beep <tshepolefera249@gmail.com>
2025-10-14 16:35:59 +0200

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -1,1 +1,5 @@
-
+import * as Sentry from '@sentry/react-native';
+Sentry.init({ dsn: 'YOUR_SENTRY_DSN' });
+export default Sentry.wrap(App);
+import analytics from '@react-native-firebase/analytics';
+await analytics().logEvent('app_open', {});
