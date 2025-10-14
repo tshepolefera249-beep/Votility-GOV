7687b380561a3c123e0e71c97a276d46ccd53425
tshepolefera249-beep <tshepolefera249@gmail.com>
2025-10-14 18:26:00 +0200

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -3,3 +3,17 @@ Sentry.init({ dsn: 'YOUR_SENTRY_DSN' });
 export default Sentry.wrap(App);
 import analytics from '@react-native-firebase/analytics';
 await analytics().logEvent('app_open', {});
+import React from 'react';
+import { ThemeProvider } from './contexts/ThemeContext';
+import { NavigationContainer } from '@react-navigation/native';
+import MainNavigator from './navigation/MainNavigator';
+
+export default function App() {
+  return (
+    <ThemeProvider>
+      <NavigationContainer>
+        <MainNavigator />
+      </NavigationContainer>
+    </ThemeProvider>
+  );
+}
